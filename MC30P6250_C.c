f6ae50588476699478547e137ab3607c08f69038
voidobserve <99012263+voidobserve@users.noreply.github.com>
2025-06-20 18:00:21 +0800

    2025-06-20-周五，存档

--- MC30P6250_C.c
+++ MC30P6250_C.c
@@ -8,44 +8,72 @@
 ******************************************************************************/
 
 #include "user.h"
+ 
+#define OSCILLATION_PIN1 (P11D)
+#define OSCILLATION_PIN2 (P14D)
+#define OSCILLATION_PIN3 (P15D)
 
-// #define OSCILLATION_PIN1    (P10D)
-// #define OSCILLATION_PIN2    (P11D)
-#define OSCILLATION_PIN1    (P11D)
-#define OSCILLATION_PIN2    (P14D)
-#define OSCILLATION_PIN3    (P15D)
+// #define OSCILLATION_PIN1 (P14D)
+// #define OSCILLATION_PIN2 (P11D)
+// #define OSCILLATION_PIN3 (P15D)
 
-#define PWM_CONTROLLER1     (P12D)
+// #define OSCILLATION_PIN1 (P15D)
+// #define OSCILLATION_PIN2 (P14D)
+// #define OSCILLATION_PIN3 (P11D)
 
-#define KEY_DETECT_INIT()   do { DDR13 = 1; P13PU = 0; } while(0)
-#define KEY_DET_PIN         (P13D)
-#define KEY_KB_INT_ENABLE   (P13KE)
-#define KEY_FILTTER_PARAM   (0b1111)
+#define PWM_CONTROLLER1 (P12D)
 
-#define RT_DETECT_ENABLE    1
+#define KEY_DETECT_INIT() \
+    do                    \
+    {                     \
+        DDR13 = 1;        \
+        P13PU = 0;        \
+    } while (0)
+#define KEY_DET_PIN (P13D)
+#define KEY_KB_INT_ENABLE (P13KE)
+#define KEY_FILTTER_PARAM (0b1111)
+
+#define RT_DETECT_ENABLE 1
 #if RT_DETECT_ENABLE
-#define RT_DETECT_INIT()    do { DDR10 = 1; P10PU = 0; P10PD = 1; } while(0)
-#define RT_DETECT_ON()      do { P10PU = 0; P10PD = 1; } while(0)   // 检测震动
-#define RT_DETECT_OFF()     do { P10PU = 1; P10PD = 0; } while(0)   // 检测充电
-#define IS_RT_DETECT()      (P10PD)
-#define RT_DETECT_UNINIT()  RT_DETECT_OFF()                     // 进入休眠前调用
-#define RT_CHARGE_DET_PIN   (P10D)
+#define RT_DETECT_INIT() \
+    do                   \
+    {                    \
+        DDR10 = 1;       \
+        P10PU = 0;       \
+        P10PD = 1;       \
+    } while (0)
+#define RT_DETECT_ON() \
+    do                 \
+    {                  \
+        P10PU = 0;     \
+        P10PD = 1;     \
+    } while (0) // 检测震动
+#define RT_DETECT_OFF() \
+    do                  \
+    {                   \
+        P10PU = 1;      \
+        P10PD = 0;      \
+    } while (0) // 检测充电
+#define IS_RT_DETECT() (P10PD)
+#define RT_DETECT_UNINIT() RT_DETECT_OFF() // 进入休眠前调用
+#define RT_CHARGE_DET_PIN (P10D)
 #endif
 
-enum {
-    MODE_FADE = 0,                  // 整体呼吸
-    MODE_ALWAYS_ON,                 // 整体常亮
-    MODE_FLASH,                     // 整体闪烁
-    MODE_TRIGER,                    // 震动闪灯
+enum
+{
+    MODE_FADE = 0,  // 整体呼吸
+    MODE_ALWAYS_ON, // 整体常亮
+    MODE_FLASH,     // 整体闪烁
+    MODE_TRIGER,    // 震动闪灯
 
-    MODE_ALWAYS_ON_LIGHT1,          // 灯1常亮
-    MODE_ALWAYS_ON_LIGHT2,          // 灯2常亮
-    MODE_FLASH_LIGHT1,              // 灯1闪烁
-    MODE_FLASH_LIGHT2,              // 灯2闪烁
-    MODE_FLASH_LIGHT1_OR_LIGHT2,    // 交替闪烁
-    MODE_FADE_LIGHT1,               // 灯1渐变
-    MODE_FADE_LIGHT2,               // 灯2渐变
-    MODE_FADE_IN_TURN,              // 同时交替渐变
+    MODE_ALWAYS_ON_LIGHT1,       // 灯1常亮
+    MODE_ALWAYS_ON_LIGHT2,       // 灯2常亮
+    MODE_FLASH_LIGHT1,           // 灯1闪烁
+    MODE_FLASH_LIGHT2,           // 灯2闪烁
+    MODE_FLASH_LIGHT1_OR_LIGHT2, // 交替闪烁
+    MODE_FADE_LIGHT1,            // 灯1渐变
+    MODE_FADE_LIGHT2,            // 灯2渐变
+    MODE_FADE_IN_TURN,           // 同时交替渐变
 
     MODE_MAX,
 };
@@ -78,49 +106,51 @@ void params_init(void);
 void delay_1ms(void)
 {
     volatile uint16_t _ms = 443;
-    while (_ms--);
+    while (_ms--)
+        ;
 }
 
 void delay_10ms(void)
 {
     volatile uint16_t _ms = 4443;
-    while (_ms--);
+    while (_ms--)
+        ;
 }
 
 /************************************************
 ;  *    @Function Name       : C_RAM
 ;  *    @Description         : 初始化RAM
-;  *    @IN_Parameter      	 : 
+;  *    @IN_Parameter      	 :
 ;  *    @Return parameter    :
 ;  ***********************************************/
 void C_RAM(void)
 {
-   	__asm 
-   	movai 0x40 
-   	movra FSR
-   	movai 48 
-   	movra 0x07 
-   	decr FSR
-   	clrr INDF
-   	djzr 0x07 
-   	goto $ -3;
-   	clrr 0x07 
-   	__endasm;
+    __asm;
+    movai 0x40;
+    movra FSR;
+    movai 48;
+    movra 0x07;
+    decr FSR;
+    clrr INDF;
+    djzr 0x07;
+    goto $ - 3;
+    clrr 0x07;
+    __endasm;
 }
 
 /************************************************
 ;  *    @Function Name       : IO_Init
-;  *    @Description         : 
-;  *    @IN_Parameter      	 : 
+;  *    @Description         :
+;  *    @IN_Parameter      	 :
 ;  *    @Return parameter    :
 ;  ***********************************************/
 void IO_Init(void)
 {
-   	P1 = 0x00; //1:input 0:output
-   	DDR1 = 0x00;   	//1:input 0:output
-   	PUCON = 0xff; //0:Effective 1:invalid
-   	PDCON = 0xff; //0:Effective 1:invalid
-    ODCON = 0x00; //0:推挽输出  1:开漏输出
+    P1 = 0x00;    // 1:input 0:output
+    DDR1 = 0x00;  // 1:input 0:output
+    PUCON = 0xff; // 0:Effective 1:invalid
+    PDCON = 0xff; // 0:Effective 1:invalid
+    ODCON = 0x00; // 0:推挽输出  1:开漏输出
 
     KEY_DETECT_INIT();
 #if RT_DETECT_ENABLE
@@ -135,9 +165,13 @@ void IO_Init(void)
 ;  ***********************************************/
 void TIMER0_INT_Init(void)
 {
-   	T0CR = 0x05; //时钟为CPU时钟    定时器64分频  1ms
-    T0CNT = 161;  // (256 - 95);
-   	T0IE = 1;
+    // FCPU 8MHz 到分频器前经过2分频，再到分频器的64分频  -> 理论上 0.000016s（16us）计数一次
+    // 但仿真上是 8us计数一次
+    // T0CR = 0x05; // 时钟为CPU时钟  预分频器分配给T0而不是WDT  定时器64分频
+    T0CR = (0x01 << 2) | (0x01 << 0); // 时钟为CPU时钟  预分频器分配给T0而不是WDT  定时器64分频
+    // T0CNT = 161;                      // (256 - 95); 递增计数器，需要手动装载
+    T0CNT = 255 - 62; // 递增计数器，需要手动装载
+    T0IE = 1;
 }
 /************************************************
 ;  *    @Function Name       : IO_Config
@@ -147,34 +181,39 @@ void TIMER0_INT_Init(void)
 ;  ***********************************************/
 void TIMER1_PWM_Init(void)
 {
-    PWM1CR0 = 0x00;     // PWM1A
-    PWM1CR1 = 0x00;     // 不取反
-    T1LOAD = 127;
-    T1CNT = 127;
-    T1DATA = 0;
-    T1DATB = 0;
-    T1CR = 0xC0;   	   	// Fcpu 1分频
+    PWM1CR0 = 0x00; // 允许PWM1A端口输出波形，T1时钟不倍频
+    PWM1CR1 = 0x00; // T1PWMA 信号电平不取反
+
+    // 理论上 0.000000125s（0.125us） 计数一次
+    T1LOAD = 124; // 124一直减到0，包括0向下溢出
+    // T1CNT = 127;
+    // T1DATA = 0;
+    // T1DATB = 0;
+    T1CR = 0xC0; // 使能定时器 Fcpu 1分频
 }
+
 /************************************************
 ;  *    @Function Name       : Sys_Init
 ;  *    @Description         : 系统初始化
-;  *    @IN_Parameter      	 : 
+;  *    @IN_Parameter      	 :
 ;  *    @Return parameter    :
 ;  ***********************************************/
 void Sys_Init(void)
 {
-   	GIE = 0;
+    GIE = 0;
     C_RAM();
     params_init();
     IO_Init();
-   	TIMER0_INT_Init();
-   	TIMER1_PWM_Init();
-   	GIE = 1;
+    TIMER0_INT_Init();
+    TIMER1_PWM_Init();
+
+    delay_1ms();
+    GIE = 1;
 }
 //=================================================================================
 /**
  * @brief 初始化所有变量，因为复位会将RAM清空，需要在清RAM后重新初始化赋值
-*/
+ */
 void params_init(void)
 {
     key_sleep_count = 0;
@@ -185,7 +224,7 @@ void params_init(void)
     key_press_10ms_count = 0;
 
     sleep_enalbe = 1;
-    light_mode = MODE_FADE;    // 上电默认模式
+    light_mode = MODE_FADE; // 上电默认模式
 }
 
 void key_scan(void)
@@ -196,7 +235,8 @@ void key_scan(void)
         key_scan_count = 0;
 
         key_filtter <<= 1;
-        if (KEY_DET_PIN) key_filtter |= 0x01;
+        if (KEY_DET_PIN)
+            key_filtter |= 0x01;
 
         key_filtter &= KEY_FILTTER_PARAM;
         if (key_filtter == KEY_FILTTER_PARAM)
@@ -209,19 +249,19 @@ void key_scan(void)
             if (key_press_10ms_count <= 200)
                 key_press_10ms_count++;
             if (key_press_10ms_count == 200)
-            {   // 长按关机
+            { // 长按关机
                 sleep_enalbe = 1;
             }
         }
         else
         {
             if (key_press_10ms_count > 20 && key_press_10ms_count < 200)
-            {   // 短按切换模式
+            { // 短按切换模式
                 light_mode++;
                 if (light_mode >= MODE_MAX)
                     light_mode = MODE_ALWAYS_ON;
 
-                {   // 初始化模式动画计数
+                { // 初始化模式动画计数
                     light_mode_flash_count = 0;
                     light_mode_flash_dir = 0;
                     light_mode_fade_dir = 0;
@@ -254,7 +294,7 @@ void light_deal_event(void)
     else if (light_mode == MODE_FLASH)
     {
         light_mode_flash_count++;
-        if (light_mode_flash_count == 393)  // 大概300ms
+        if (light_mode_flash_count == 393) // 大概300ms
         {
             light_mode_flash_count = 0;
             light_mode_flash_dir ^= 1;
@@ -352,7 +392,7 @@ void light_deal_event(void)
     else if (light_mode == MODE_FLASH_LIGHT1)
     {
         light_mode_flash_count++;
-        if (light_mode_flash_count == 393)  // 大概300ms
+        if (light_mode_flash_count == 393) // 大概300ms
         {
             light_mode_flash_count = 0;
             light_mode_flash_dir ^= 1;
@@ -370,7 +410,7 @@ void light_deal_event(void)
     else if (light_mode == MODE_FLASH_LIGHT2)
     {
         light_mode_flash_count++;
-        if (light_mode_flash_count == 393)  // 大概300ms
+        if (light_mode_flash_count == 393) // 大概300ms
         {
             light_mode_flash_count = 0;
             light_mode_flash_dir ^= 1;
@@ -388,7 +428,7 @@ void light_deal_event(void)
     else if (light_mode == MODE_FLASH_LIGHT1_OR_LIGHT2)
     {
         light_mode_flash_count++;
-        if (light_mode_flash_count == 393)  // 大概300ms
+        if (light_mode_flash_count == 393) // 大概300ms
         {
             light_mode_flash_count = 0;
             light_mode_flash_dir++;
@@ -517,7 +557,7 @@ void light_deal_event(void)
 
     T1DATA = pwm_duty;
     timer1_count = 0;
-    T1IE = 1;  // 开启Timer1计数中断，配合计数28个PWM周期后自动将PWM占空比设置为0
+    T1IE = 1; // 开启Timer1计数中断，配合计数28个PWM周期后自动将PWM占空比设置为0
 }
 
 #if RT_DETECT_ENABLE
@@ -534,26 +574,26 @@ void rt_detect_event(void)
         if (switch_mode_count == 0)
         {
             if (IS_RT_DETECT())
-            {   // 切换到充电检测模式
+            { // 切换到充电检测模式
                 RT_DETECT_OFF();
                 switch_mode_count = 3;
             }
             else
-            {   // 切换到震动检测模式
+            { // 切换到震动检测模式
                 RT_DETECT_ON();
                 switch_mode_count = 40;
             }
-            return;  // 跳过这个周期，让上/下拉电阻有时间稳定电压
+            return; // 跳过这个周期，让上/下拉电阻有时间稳定电压
         }
     }
     else
-    {   // 一般不会进入这里，添加防止意外导致无法切换检测模式
+    { // 一般不会进入这里，添加防止意外导致无法切换检测模式
         switch_mode_count = 1;
         return;
     }
 
-    if (IS_RT_DETECT()) 
-    {   // 震动检测
+    if (IS_RT_DETECT())
+    { // 震动检测
         switch (Statue)
         {
         case 0:
@@ -564,7 +604,7 @@ void rt_detect_event(void)
             break;
 
         case 1:
-            if (RT_CHARGE_DET_PIN)  // 由低电平转为高电平加1
+            if (RT_CHARGE_DET_PIN) // 由低电平转为高电平加1
             {
                 Statue = 0;
                 pluse_count++;
@@ -582,7 +622,7 @@ void rt_detect_event(void)
             break;
         }
 
-        if (pluse_count >= 3)//lmd_buf[sys_ctl.lmd_num-1])//灵敏度调节参数,越少越敏感
+        if (pluse_count >= 3) // lmd_buf[sys_ctl.lmd_num-1])//灵敏度调节参数,越少越敏感
         {
             pluse_count = 0;
             pluse_time = 0;
@@ -592,7 +632,7 @@ void rt_detect_event(void)
         }
     }
     else
-    {   // 充电检测
+    { // 充电检测
         if (RT_CHARGE_DET_PIN)
             sleep_enalbe = 1;
     }
@@ -602,12 +642,14 @@ void rt_detect_event(void)
 void main(void)
 {
     Sys_Init();
-    
+
     while (1)
     {
         // __asm;
         // clrwdt;
         // __endasm;
+
+#if 1
 
         if (timer0_flag)
         {
@@ -618,7 +660,6 @@ void main(void)
             rt_detect_event();
 #endif
         }
-        
 
         if (sleep_enalbe)
         {
@@ -638,7 +679,7 @@ void main(void)
 
             key_sleep_count = 0;
             while (1)
-            {   // 等待松手
+            { // 等待松手
                 // __asm;
                 // clrwdt;
                 // __endasm;
@@ -657,7 +698,7 @@ void main(void)
                     key_sleep_count = 0;
                 }
             }
-            
+
             key_sleep_count = 1;
             while (1)
             {
@@ -714,11 +755,13 @@ void main(void)
 #endif
             KEY_KB_INT_ENABLE = 0;
             KBIE = 0;
-   	   	   	KBIF = 0;
+            KBIF = 0;
             T1EN = 1;
             GIE = 1;
         }
-   	}
+
+#endif
+    }
 }
 
 /************************************************
@@ -729,16 +772,27 @@ void main(void)
 ;  ***********************************************/
 void int_isr(void) __interrupt
 {
-   	__asm 
-   	movra _abuf   
-   	swapar _STATUS
-   	movra _statusbuf
-   	__endasm;
+    __asm;
+    movra _abuf;
+    swapar _STATUS;
+    movra _statusbuf;
+    __endasm;
 
-   	if ((T1IF) && (T1IE))
-   	{
+    if ((T0IF) && (T0IE))
+    {
+        T0IF = 0;
+        // T0CNT = 161; // (256 - 95);
+        T0CNT = 255 - 62; // 递增计数器，需要手动装载
+
+        // P12D = ~P12D; // 测试中断时间
+
+        timer0_flag = 1;
+    }
+
+    if ((T1IF) && (T1IE))
+    {
         T1IF = 0;
-        
+
         timer1_count++;
         if (timer1_count > 28)
         {
@@ -746,26 +800,18 @@ void int_isr(void) __interrupt
             T1DATA = 0;
         }
     }
-    
-    if ((T0IF) && (T0IE))
-   	{
-   	   	T0IF = 0;
-   	   	T0CNT = 161;  // (256 - 95);
 
-        timer0_flag = 1;
+    if ((CMPIE) && (CMPIF))
+    {
+        CMPIF = 0;
     }
 
-   	if ((CMPIE) && (CMPIF))
-   	{
-   	    CMPIF = 0;
-    }
-    __asm
-   	swapar _statusbuf
-   	movra _STATUS
-   	swapr _abuf
-   	swapar _abuf
-   	__endasm;
+    __asm;
+    swapar _statusbuf;
+    movra _STATUS;
+    swapr _abuf;
+    swapar _abuf;
+    __endasm;
 }
-
 
 /**************************** end of file *********************************************/
